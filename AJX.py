4f0bcb60ad43e010ab20b1b37836ea5b4d2ddcbc
xc-link <cai_liyong@qq.com>
2024-10-24 06:41:45 +0800

    Create AJX.py

---
+++ AJX.py
@@ -0,0 +1,6 @@
+keyauthapp = api(
+    name = "AJX", # App name - VaultCord.com FREE Discord backup bot for members & your entire server saved from terms and nukes!
+    ownerid = "o0dHuOg0Bv", # Account ID
+    version = "1.0", # Application version. Used for automatic downloads see video here https://www.youtube.com/watch?v=kW195PLCBKs
+    hash_to_check = getchecksum()
+)
